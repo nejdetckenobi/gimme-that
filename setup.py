6396fa7f128fb4fc6cc8539986375dd2d68e7340
nejdetckenobi <nejdetyucesoy@gmail.com>
2018-02-04 12:44:38 +0300

    Drag and drop support added. Fixes #4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='GimmeThat',
-    version='0.2.1',
+    version='0.2.2',
     description='A file transfer tool',
     url='https://github.com/nejdetckenobi/gimme-that',
     author='nejdetckenobi',
