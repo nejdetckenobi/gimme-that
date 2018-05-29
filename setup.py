4897c129f070fdccaea04161f01eff87e72fa6a8
nejdetckenobi <nejdetyucesoy@gmail.com>
2018-05-29 15:30:48 +0300

    Parameters fixed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='GimmeThat',
-    version='2.0',
+    version='2.0.1',
     description='A file transfer tool',
     url='https://github.com/nejdetckenobi/gimme-that',
     author='nejdetckenobi',
